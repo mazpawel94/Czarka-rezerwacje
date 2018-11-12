7f097723021902cf4a40773c373101da6ae61bca
mazpawel94 <mazpawel94@gmail.com>
2018-11-12 14:04:38 +0100

    change styles

--- db.js
+++ db.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
+//  const mongodb = require('mongodb');
+//  const MongoClient = mongodb.MongoClient;
+
 mongoose.connect('mongodb://testuser:testuser1@ds157843.mlab.com:57843/heroku_3sg9gr36', function(){
 // mongoose.connect('mongodb://localhost/reservation', function(){
     console.log('połączenie nawiązane');
 })
 module.exports = mongoose;
+// module.exports = mongodb;
